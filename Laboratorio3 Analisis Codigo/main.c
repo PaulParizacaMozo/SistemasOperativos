6a261ba5e7a9ab85f4c6aa7ac8b13864fdb94773
PaulParizacaMozo <pparizaca@unsa.edu.pe>
2023-09-29 07:31:16 -0500

    ejer1

--- Laboratorio3 Analisis Codigo/main.c
+++ Laboratorio3 Analisis Codigo/main.c
@@ -2,18 +2,23 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 int main(void) {
-   pid_t pid;
-   int var= 0;
-   printf("PID antes de fork(): %d\n", (int) getpid());
-   if ( (pid = fork()) > 0 ) {
-      printf ("PID del padre: %d\n", (int) getpid());
-      var++;
-   } else {
-      if (pid == 0) 
-         printf ("PID del hijo: %d\n", (int) getpid());
-      else
-         printf ("Error al hacer fork()\n");
-   }
-   printf("Proceso [%d] -> var = %d\n", (int) getpid(), var);
+    pid_t pid;  // Declaración de una variable para almacenar el ID del proceso
+    int var = 0;  // Declaración de una variable entera llamada "var" e inicialización a 0
+
+    printf("PID antes de fork(): %d\n", (int) getpid()); // Imprimir el ID del proceso antes de crear un proceso hijo
+    getchar();
+    if ((pid = fork()) > 0) { // Crear un proceso hijo y verificar si se está ejecutando en el proceso padre
+        printf("PID del padre: %d\n", (int) getpid()); // Imprimir el ID del proceso padre
+        var++; // Incrementar la variable "var" en el proceso padre
+    } else {
+        if (pid == 0) // Verificar si se está ejecutando en el proceso hijo
+            printf("PID del hijo: %d\n", (int) getpid()); // Imprimir el ID del proceso hijo
+        else
+            printf("Error al hacer fork()\n"); // En caso de error al crear el proceso hijo
+    }
+
+    printf("Proceso [%d] -> var = %d\n", (int) getpid(), var); // Imprimir el ID del proceso actual y el valor de "var"
+    getchar();
 }
