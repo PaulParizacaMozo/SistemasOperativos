d40700fc2a56defbe55739673913498509d23ba8
PaulParizacaMozo <pparizaca@unsa.edu.pe>
2023-11-23 12:04:23 -0500

    lab07 - roundrobin

--- Laboratorio_07/round_robin.c
+++ Laboratorio_07/round_robin.c
@@ -19,19 +19,18 @@ void roundRobin(Proceso procesos[], int n_procesos, int quantum) {
         procesos[i].tiempoRestante = procesos[i].tiempoServico;
     }
 
-    // Ejecutar el algoritmo Round Robin
     printf("Tiempo\t\tProceso\n");
+    // Inicio del algoritmo Round Robin
     int tiempoActual = 0;
     while (tiempoActual < tiempoTotal) { //Ejecucion hasta que cumpla todos los procesos
         for (int i = 0; i < n_procesos; i++) { // Recorrido de proceso en proceso
             if (procesos[i].tiempoRestante > 0) { // Comprueba si el procesos esta completado
                 // Calcula el tiempo restante para el procesos actual
-                int tiempoServico = (procesos[i].tiempoRestante < quantum) ? procesos[i].tiempoRestante : quantum;
-                procesos[i].tiempoRestante -= tiempoServico;
-                tiempoActual += tiempoServico;
+                int tiempoServico = (procesos[i].tiempoRestante < quantum) ? procesos[i].tiempoRestante : quantum; // Calculo del valor a restar al tiempo restante
+                procesos[i].tiempoRestante -= tiempoServico; //Actualizacion de tiempo restante
+                tiempoActual += tiempoServico; // Actualizacion del tiempo actual recorrido
                 // Imprime el proceso actual y la informacion del tiempo recorrido
                 printf("%d -> %d\t\t%s\n", tiempoActual - tiempoServico, tiempoActual, procesos[i].id);
-
                 // Si un proceso termina mostrar el tiempo que demora en completarse.
                 if (procesos[i].tiempoRestante == 0) {
                     printf(">>> Proceso %s completado. Tiempo de total de ejecucion(retorno): %d\n\n", procesos[i].id, tiempoActual);
